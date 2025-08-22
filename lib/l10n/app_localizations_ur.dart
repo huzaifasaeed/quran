ae4a3cbbe20d38c8ffcbc00e618a9f0e4c1b79c2
huzaifa saeed <huzaifa.saeed@breakthru.pk>
2025-08-22 16:55:48 +0500

    urdu translation added

---
+++ lib/l10n/app_localizations_ur.dart
@@ -0,0 +1,332 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Urdu (`ur`).
+class AppLocalizationsUr extends AppLocalizations {
+  AppLocalizationsUr([String locale = 'ur']) : super(locale);
+
+  @override
+  String get more => 'مزید';
+
+  @override
+  String get quran => 'قرآن';
+
+  @override
+  String get search => 'تلاش کریں';
+
+  @override
+  String get surah => 'سورة';
+
+  @override
+  String get juz => 'پارہ';
+
+  @override
+  String get hizb => 'حزب';
+
+  @override
+  String get bookmark => 'نشانی';
+
+  @override
+  String get sajda => 'سجدة';
+
+  @override
+  String get ayat => 'آیت';
+
+  @override
+  String get favorites => 'پسندیدہ';
+
+  @override
+  String get language => 'زبان';
+
+  @override
+  String get helpGuide => 'ہدایات';
+
+  @override
+  String get introduction => 'تعارف';
+
+  @override
+  String get shareApp => 'ایپ شیئر کریں۔';
+
+  @override
+  String get references => 'حوالہ جات';
+
+  @override
+  String get switchTheme => 'ڈیزائن تبدیل کریں۔';
+
+  @override
+  String get lightMode => 'سفید کریں';
+
+  @override
+  String get darkMode => 'سیاہ کریں';
+
+  @override
+  String get quranMode => 'قرآن';
+
+  @override
+  String get greenMode => 'سبز کریں';
+
+  @override
+  String get translation => 'ترجمہ';
+
+  @override
+  String get selectedTranslation => 'منتخب ترجمہ';
+
+  @override
+  String get reading => 'پڑھیں';
+
+  @override
+  String get searchSurah => 'سورة تلاش کریں۔';
+
+  @override
+  String get madeByFabrikod => 'made by fabrikod';
+
+  @override
+  String get version => ' - V';
+
+  @override
+  String get copy => 'کاپی';
+
+  @override
+  String get copied => 'کاپی ہو گئی';
+
+  @override
+  String get share => 'شیئر کریں';
+
+  @override
+  String get scroll => 'مزید';
+
+  @override
+  String get mushaf => 'مصحف';
+
+  @override
+  String get quranType => 'قرآن کی قسم';
+
+  @override
+  String get readingStyle => 'پڑھنے کا انداز';
+
+  @override
+  String get all => 'تمام';
+
+  @override
+  String get arabic => 'عربی';
+
+  @override
+  String get fontType => 'Font Type';
+
+  @override
+  String get helpGuideTitle1 => 'What is Qur\'an Kareem';
+
+  @override
+  String get helpGuideDescription1 =>
+      'The Holy Quran is the religious text of Islam, believed by Muslims to be the word of God as revealed to Prophet Muhammad through the angel Gabriel. It contains guidance and teachings on various aspects of life, including faith, morality, spirituality, and daily living.';
+
+  @override
+  String get helpGuideTitle2 =>
+      'How many chapters does the Qur’an Kareem include?';
+
+  @override
+  String get helpGuideDescription2 => '114 chapters';
+
+  @override
+  String get helpGuideTitle3 => 'Can I read separately versicles and chapters?';
+
+  @override
+  String get helpGuideDescription3 =>
+      'Yes you can. You can jump from one page to another in the way you prefer. You can also mark them so you can directly read them without searching them in the whole book.';
+
+  @override
+  String get helpGuideTitle4 => 'Can I share the content in other places?';
+
+  @override
+  String get helpGuideDescription4 =>
+      'Yes you can, Open Quran is an open source app where you are free to use the content.';
+
+  @override
+  String get helpGuideTitle5 => 'What is the purpose of the app?';
+
+  @override
+  String get helpGuideDescription5 =>
+      'The main purpose of the app is to offer to the community a native open source mobile app where they can read, listen and save their surahs, ayas or juzs.';
+
+  @override
+  String get helpGuideTitle6 => 'What are the key features of the app?';
+
+  @override
+  String get helpGuideDescription6 =>
+      'You can read with no limit the Holy Qur’an, you mark with the bookmarks the last aya you read so you won’t get lost next time you open your Qur’an. You can listen the ayas in case you don’t know how to read it in Arabic and also you can save your most read surahs, ayas or juzs.';
+
+  @override
+  String get helpGuideTitle7 =>
+      'How can users troubleshoot problems and find solutions? ';
+
+  @override
+  String get helpGuideDescription7 =>
+      'You can always contact us via email: merhaba@fabrikod.com';
+
+  @override
+  String get helpGuideTitle8 =>
+      'Are there any known limitations or restrictions in the app?';
+
+  @override
+  String get helpGuideDescription8 =>
+      'No there is not, you can read and listen as much as you want.';
+
+  @override
+  String get helpGuideTitle9 => 'Who is the target audience for the app?';
+
+  @override
+  String get helpGuideDescription9 =>
+      'Everyone who has an interest in getting to know the Holy Qur’an, reading or listen to it. Everyone is welcome.';
+
+  @override
+  String get nextPage => 'Next Page';
+
+  @override
+  String get page => 'صفحہ';
+
+  @override
+  String get previousPage => 'Previous Page';
+
+  @override
+  String get surahs => 'Surahs';
+
+  @override
+  String get noResultsFound =>
+      'No results found\nTry searching for a different keyword';
+
+  @override
+  String get suggestions => 'Suggestions';
+
+  @override
+  String get noBookMarksAdded => 'No Bookmarks Added';
+
+  @override
+  String get noFavoritesAdded => 'No Favorites Added';
+
+  @override
+  String get and => 'And';
+
+  @override
+  String get theOpenQuran => 'The Open Quran';
+
+  @override
+  String get bookmarks => 'Bookmarks';
+
+  @override
+  String get recent => 'Recent';
+
+  @override
+  String get read => 'Read';
+
+  @override
+  String get fontSize => 'Font Size';
+
+  @override
+  String get layout => 'Layout';
+
+  @override
+  String get sound => 'Sound';
+
+  @override
+  String get backgroundColor => 'Background Color';
+
+  @override
+  String get ayatAndTranslation => 'Ayat + Translation';
+
+  @override
+  String get hide => 'Hide';
+
+  @override
+  String get show => 'Show';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get rate => 'Rate';
+
+  @override
+  String get writeAnAppStoreReview => 'Write an app store review';
+
+  @override
+  String get play => 'Play';
+
+  @override
+  String get pause => 'Pause';
+
+  @override
+  String get favorite => 'Favorite';
+
+  @override
+  String get meaning => 'Meaning';
+
+  @override
+  String get navigation => 'Navigations';
+
+  @override
+  String get searchSurahJuzOrAyahs => 'Search surah, juz or ayahs...';
+
+  @override
+  String get searchSurahJuzOrPage => 'Search for surah, juz or page...';
+
+  @override
+  String get searchFor => 'Search for';
+
+  @override
+  String get reciter => 'Reciter';
+
+  @override
+  String get download => 'Download';
+
+  @override
+  String get speed => 'Speed';
+
+  @override
+  String get downloaded => 'Downloaded';
+
+  @override
+  String get delete => 'Delete';
+
+  @override
+  String get quranFont => 'Qur\'an Font';
+
+  @override
+  String get availableToDownload => 'Available to Download';
+
+  @override
+  String get fabrikodTwoThree => '@2023 Fabrikod';
+
+  @override
+  String get referencesDescription =>
+      'The goal of the project is to provide people with a convenient platform for studying the Holy Qur\'an. Therefore, the Qur\'an can now be read on smartphones, tablets and other modern devices. To see the source code for this app, please visit the ';
+
+  @override
+  String get referencesDescription2 =>
+      ' As explained in our Privacy Policy we do not collect any personal information.';
+
+  @override
+  String get referencesDescription3 =>
+      'The API and all Qur\'an sources are used from ';
+
+  @override
+  String get contactUs => 'Contact us at merhaba@fabrikod.com for assistance.';
+
+  @override
+  String get privacyPolicy => 'Privacy policy';
+
+  @override
+  String get nextSurah => 'Next Surah';
+
+  @override
+  String get previousSurah => 'Previous Surah';
+
+  @override
+  String get beggingOfSurah => 'Begging Of Surah';
+
+  @override
+  String get openSourceDevelopedByFabrikod =>
+      'open source developed by Huzaifa Saeed';
+}
